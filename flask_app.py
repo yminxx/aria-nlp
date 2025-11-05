17e4782929e935d69c22956b135e75e4ddeb4134
ysaa <ysagabrielle.cervantes@gmail.com>
2025-11-05 14:22:03 +0800

    edit2

--- flask_app.py
+++ flask_app.py
@@ -750,5 +750,6 @@ def check_compat():
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("FLASK_RUN_PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
